e22d1e70415032cd0a76d93b3731f7ec39fa04af
Phil Owen <phillipsowen@outlook.com>
2020-04-07 07:51:32 -0400

    making ready for prod deployment

--- Ventilator/urls.py
+++ Ventilator/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', app_views.index, name='index'),
     path('admin/', admin.site.urls),
     url(r'^index', app_views.index, name='index'),
 ]
