8f33e5dba77bffabf920b6aaafb36a598bfcce2f
Phil Owen <phillipsowen@outlook.com>
2020-04-09 14:05:34 -0400

    changing names of some html tags, adding the data model.

--- RENCI_Ventilator/models.py
+++ RENCI_Ventilator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class pressure(models.Model):
+    id =  models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    value = models.FloatField()
+
+class repiration(models.Model):
+    id =  models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    value = models.FloatField()
+
+class configuration(models.Model):
+    id =  models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 150)
+    value = models.FloatField()
+
