d2e3c44d1c55b71aacd0fe84b0244d2b70c01b56
Phil Owen <phillipsowen@outlook.com>
2020-04-09 14:42:35 -0400

    adding controls to the diagnostic and calibration tabs

--- RENCI_Ventilator/models.py
+++ RENCI_Ventilator/models.py
@@ -2,17 +2,32 @@ from django.db import models
 
 class pressure(models.Model):
     id =  models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 50)
+    sensor_name = models.CharField(max_length = 50)
     value = models.FloatField()
+    ts = models.DateTimeField(null = True)
 
-class repiration(models.Model):
+class repiration_rate(models.Model):
     id =  models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 50)
     value = models.FloatField()
+    ts = models.DateTimeField(null = True)
 
 class configuration(models.Model):
     id =  models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 50)
+    param_name = models.CharField(max_length = 50)
     description = models.CharField(max_length = 150)
     value = models.FloatField()
+    ts = models.DateTimeField(null=True)
 
+class calibration(models.Model):
+    id =  models.AutoField(primary_key=True)
+    param_name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 150)
+    value = models.FloatField()
+    ts = models.DateTimeField(null=True)
+
+class diagnostics(models.Model):
+    id =  models.AutoField(primary_key=True)
+    param_name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 150)
+    value = models.FloatField()
+    ts = models.DateTimeField(null=True)
