1b4093a2848d0a21cb12e7234ddc05118276ee9e
Phil Owen <phillipsowen@outlook.com>
2020-04-13 12:09:17 -0400

    updates to clean up code, comments

--- RENCI_Ventilator/sensor.py
+++ RENCI_Ventilator/sensor.py
@@ -1,67 +1,71 @@
 import time
 
-# set the operational mode
-DEBUG = True
 
 # provides access to the sensor data
 class SensorHandler:
-    standard_units = True
-
-    class debug_bmp:
-        sea_level_pressure = 0
-        pressure = 0
-        temperature = 0
-        altitude = 0
-        bmp = None
-
-        # init the class variables
+    # debug class that simulates the real sensor
+    class DebugBmp:
+        # init the debug simulator class
         def __init__(self):
             import random
             self.pressure = random.randrange(975, 1002, 1)
             self.temperature = random.randrange(20, 30, 1)
             self.altitude = random.randrange(220, 230, 1)
 
-    def __init__(self, sea_level_pressure, standard_units: bool = True):
+    # init the SensorHandler class
+    def __init__(self, debug_mode: False, sea_level_pressure: float = 1000.8, standard_units: bool = True):
         # if we are not in debug mode setup the raspberry pi
-        if not DEBUG:
+        if not debug_mode:
             import board
             import busio
             import adafruit_bmp3xx
 
-            # I2C setup
+            # i2c init
             i2c = busio.I2C(board.SCL, board.SDA)
             self.bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
-            # set the sampling rates
+            # set the sensor sampling rates
             self.bmp.pressure_oversampling = 8
             self.bmp.temperature_oversampling = 2
         else:
-            self.bmp = self.debug_bmp()
+            self.bmp = self.DebugBmp()
 
         # init the other params
         self.bmp.sea_level_pressure = sea_level_pressure
         self.standard_units = standard_units
 
+    #################
+    # declare methods that will get sensor data in selected units
+    #################
+    # get pressure
     def get_pressure(self):
+        # in standard mode return psi
         if self.standard_units:
             return self.get_psi_pressure()
+        # else return hpa
         else:
             return self.get_hpa_pressure()
 
+    # get temperature
     def get_temperature(self):
+        # return Fahrenheit
         if self.standard_units:
             return self.get_fahrenheit()
+        # return Celsius
         else:
             return self.get_celsius()
 
+    # get altitude
     def get_altitude(self):
+        # return feet
         if self.standard_units:
             return self.get_altitude_feet()
+        # return meters
         else:
             return self.get_altitude_meter()
 
     #################
-    # return standard units
+    # return sensor readings in standard units
     #################
 
     # get pressure in psi
@@ -82,16 +86,17 @@ class SensorHandler:
         return round(self.bmp.altitude * 3.28, 2)
 
     #################
-    # return metric units
+    # return sensor readings in metric units
     #################
-
     # get pressure in hPa
     def get_hpa_pressure(self):
         return round(self.bmp.pressure, 2)
 
+    # return the temperature in celsius
     def get_celsius(self):
         return round(self.bmp.temperature, 2)
 
+    # return the altitude in meeters
     def get_altitude_meter(self):
         return round(self.bmp.altitude, 2)
 
@@ -99,13 +104,23 @@ class SensorHandler:
     def set_sea_level_pressure(self, sea_level_pressure):
         self.bmp.sea_level_pressure = sea_level_pressure
 
+# debug testing
 if __name__ == '__main__':
-    sh = SensorHandler(1001.8)
+    # fire up the class to read the sensor
+    sh = SensorHandler(True)
 
-    while (1):
-        temperature = sh.get_temperature()
+    # forever
+    while True:
+        # get the temperature
+        temperature = sh.get_temperature(True)
+
+        # get the altitude
         altitude = sh.get_altitude()
+
+        # get the ambient pressure in psi
         psi = sh.get_pressure()
 
         print(f'Pressure: {psi}  Temperature: {temperature}  Altitude (M): {altitude}')
-        time.sleep(1)
+
+        # do it again in a second
+        time.sleep(2)
